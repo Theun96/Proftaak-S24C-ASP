fa10f5730862bd09c373258c685e2aa45b92a949
Theun Schut <theun_96@hotmail.com>
2015-12-03 13:39:07 +0100

    beheer

--- ICT4Rails/Beheer.aspx.cs
+++ ICT4Rails/Beheer.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,9 @@ namespace ICT4Rails
 {
     public partial class Beheer : System.Web.UI.Page
     {
+        //public List<Rail> Rails { get; private set; }
+        //public List<Sector> Sectors { get; private set; }
+
         protected void Page_Load(object sender, EventArgs e)
         {
             tblBeheer.Width = new Unit("100%");
@@ -33,6 +37,66 @@ namespace ICT4Rails
                 }
                 tblBeheer.Rows.Add(r);
             }
+        }
+
+        public void UpdateGrid()
+        {
+            //tlpGrid.Visible = false;
+            //tlpGrid.Controls.Clear();
+            /*
+            foreach (Sector s in Sectors)
+            {
+                Label l = s.AddSectorLabel(this);
+
+                string columnString = l.Tag.ToString();
+                string rowString = l.Tag.ToString();
+
+                int spaceIndex = columnString.IndexOf(" ");
+
+                columnString = columnString.Substring(0, spaceIndex);
+                rowString = rowString.Substring(spaceIndex);
+
+                int column = Convert.ToInt32(columnString);
+                int row = Convert.ToInt32(rowString);
+
+                //tlpGrid.Controls.Add(l, column, row);
+            }
+
+            foreach (Rail r in Rails)
+            {
+                Label l = r.AddRailLabel();
+
+                string columnString = l.Tag.ToString();
+                string rowString = l.Tag.ToString();
+
+                int spaceIndex = columnString.IndexOf(" ");
+
+                columnString = columnString.Substring(0, spaceIndex);
+                rowString = rowString.Substring(spaceIndex);
+
+                int column = Convert.ToInt32(columnString);
+                int row = Convert.ToInt32(rowString);
+
+                tlpGrid.Controls.Add(l, column, row);
+            }
+            */
+            //tlpGrid.Visible = true;
+        }
+
+        /// <summary>
+        /// Get all the Rails from the database and place them in a list of Rails
+        /// </summary>
+        private void GetAllRails()
+        {
+            //DataTable DT = DatabaseManager.ExecuteReadQuery(DatabaseQuerys.query["GetAllRails"], null);
+            /*
+            int railID;
+
+            foreach (DataRow DR in DT.Rows)
+            {
+                railID = Convert.ToInt32(DR["RAILID"]);
+                Rails.Add(new Rail(railID));
+            }*/
         }
     }
 }
