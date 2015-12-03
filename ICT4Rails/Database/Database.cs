38c3a44b664fabf9f38590372c461140f0f461c9
i318583 <l.vankeulen@student.fontys.nl>
2015-12-03 09:52:26 +0100

    Database class etc

---
+++ ICT4Rails/Database/Database.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Oracle.ManagedDataAccess.Client;
+
+namespace ICT4Rails.Database
+{
+    public class Database
+    {
+        private OracleConnection connection;
+        public OracleConnection Connection
+        {
+            get
+            {
+                connection = new OracleConnection();
+                try
+                {
+                    connection.Open();
+                }
+                catch
+                {
+                    HttpContext.Current.Response.Redirect("UserPage.aspx", true);
+                }
+                return connection;
+            }
+        }
+    }
+}
---
+++ ICT4Rails/DatabaseError.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DatabaseError.aspx.cs" Inherits="ICT4Rails.DatabaseError" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Error connecting to the database</title>
+</head>
+<body>
+    There was an error connecting to the database.
+</body>
+</html>
