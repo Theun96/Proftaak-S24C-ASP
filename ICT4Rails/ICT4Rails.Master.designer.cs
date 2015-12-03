3deb3c80971a2e54af616b7e4a30ad54a149e001
Theun Schut <theun_96@hotmail.com>
2015-12-03 09:31:25 +0100

    netjes zetten master page

--- ICT4Rails/ICT4Rails.Master.designer.cs
+++ ICT4Rails/ICT4Rails.Master.designer.cs
@@ -31,12 +31,12 @@ namespace ICT4Rails {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
-        /// MainContent control.
+        /// ContentPlaceHolder1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder MainContent;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
     }
 }
