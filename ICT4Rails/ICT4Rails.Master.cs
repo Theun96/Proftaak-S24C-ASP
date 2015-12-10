56c20155e2e94f9ae54d582156bfed0090e75f88
i318583 <l.vankeulen@student.fontys.nl>
2015-12-10 10:18:30 +0100

    FIXED BRAMS AMATEURISME

--- ICT4Rails/ICT4Rails.Master.cs
+++ ICT4Rails/ICT4Rails.Master.cs
@@ -11,7 +11,10 @@ namespace ICT4Rails
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (Session["User"] != null)
+            {
+                
+            }
         }
     }
 }
