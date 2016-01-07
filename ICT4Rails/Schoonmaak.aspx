db409b7e8cc656debce4db1555762bc12c813073
Boer,Bram B.P. de <bram.deboer@student.fontys.nl>
2016-01-07 10:13:36 +0100

    dropdowns gefixt

--- ICT4Rails/Schoonmaak.aspx
+++ ICT4Rails/Schoonmaak.aspx
@@ -20,7 +20,7 @@
         </div>
         <div class="col-md-3  col-md-offset-1">
             <asp:DropDownList ID="DropDownUsers" runat="server" cssclass="form-control dropdown"></asp:DropDownList>
-            <asp:Button ID="ButtonCleaned" runat="server" Text="Gerepareerd" cssclass="form-control" />
+            <asp:Button ID="ButtonCleaned" runat="server" Text="Schoongemaakt" cssclass="form-control" />
         </div>
         <div class="col-md-3  col-md-offset-4">
             <input type="text" value="1/1/2016" readonly="readonly" name="datepicker" id="datepicker" class="datepicker form-control"/>
--- ICT4Rails/Techniek.aspx.cs
+++ ICT4Rails/Techniek.aspx.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ICT4Rails.Classes;
+using ICT4Rails.Data_Layer;
 
 namespace ICT4Rails
 {
@@ -11,7 +14,12 @@ namespace ICT4Rails
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            DataTable dt = DatabaseManager.ExecuteReadQuery(DatabaseQuerys.Query["GetAllEngineers"], null);
 
+            foreach (DataRow dr in dt.Rows)
+            {
+                DropDownUsers.Items.Add(dr["NAAM"].ToString());
+            }
         }
 
         protected void ButtonRepaired_Click(object sender, EventArgs e)
